74e8c735f48f40c5015e9fe7bc643d4eb23c5269
NimbusNaut <jquin2010@gmail.com>
2026-04-24 12:18:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+function launchGame(url) {
+  const modal = document.getElementById("gameModal");
+  const frame = document.getElementById("gameFrame");
 
+  frame.src = url;
+  modal.style.display = "block";
+}
+
+function closeGame() {
+  const modal = document.getElementById("gameModal");
+  const frame = document.getElementById("gameFrame");
+
+  modal.style.display = "none";
+  frame.src = "";
+}
